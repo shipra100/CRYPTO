faf42cb8610dcff883cade89e4c28cc3238c5d66
shipra100 <luckymaddhesiya344@gmail.com>
2024-11-13 18:10:49 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import Coin from './pages/Home/Coin/Coin.jsx'
 import CoinContextProvider from './context/CoinContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <CoinContextProvider >
     <App />
     </CoinContextProvider >
-    </BrowserRouter>
+    </HashRouter>
    
   </React.StrictMode>,
 )
